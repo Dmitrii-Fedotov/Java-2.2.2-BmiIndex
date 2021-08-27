52df61a1e04ed1f9aa0b1dff179d65f9ff157f88
Dmitrii-Fedotov <87369444+Dmitrii-Fedotov@users.noreply.github.com>
2021-08-27 10:53:49 +0300

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,9 @@
 public class BmiService {
 
-    public int calculate(int weight, double height) {
+    public double calculate(int weight, double height) {
 
         double bmi = weight / (height * height);
 
-        return (int) bmi;
+        return bmi;
     }
 }
